03d56e34a4d87aca12bc15536b51de881003e22e
AdityaCoder-N <91337266+AdityaCoder-N@users.noreply.github.com>
2022-08-16 13:20:43 +0530

    final

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ connectToMongo();
 const app = express()
 const port = process.env.PORT || 3001;
 
-app.use(cors(
-  {
-    origin:"*",
-  }
-))
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin",'*');
+  res.header("Access-Control-Allow-Credentials",true);
+  res.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers",'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+
 app.use(express.json())
 //available routes
 
