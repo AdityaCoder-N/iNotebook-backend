1550d674c34c61be290c37639d93d03f4c046e03
AdityaCoder-N <91337266+AdityaCoder-N@users.noreply.github.com>
2022-08-16 13:42:39 +0530

    final

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin
 next();
 });
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if ('OPTIONS' == req.method) {
+  res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(express.json())
 //available routes
