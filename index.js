dbdc19fa4cb69b06cc16c555217467ac900f4d18
AdityaCoder-N <91337266+AdityaCoder-N@users.noreply.github.com>
2022-08-16 17:32:12 +0530

    commit

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ app.use(cors({
   origin:"*",
 }))
 
-app.use(function(req,res,next){
-  res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Credentials", "true");
-res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-next();
-});
-
+// app.use(function(req,res,next){
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+//   next();
+// });
 
 app.use(express.json())
+
 //available routes
 
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/notes',require('./routes/notes'));
+app.use('/api/auth',require('./routes/auth')); // for login signup authentication
+app.use('/api/notes',require('./routes/notes')); // notes of user
 
 if(process.env.NODE_ENV == "production"){
   app.use(express.static("client/build"));
